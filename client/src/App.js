2d70876678231615d12c7fa5d3f19cdd99320484
coriagogo <pkcparis@gmail.com>
2019-06-14 12:39:18 -0400

    self study answers added

--- client/src/App.js
+++ client/src/App.js
@@ -9,21 +9,30 @@ import JokesList from './components/JokesList';
 import './App.css';
 
 
-function App() {
-  return (
-    <div className="App">
-      <nav>
-        <NavLink to='/jokes'>Jokes</NavLink>
-        <NavLink to='/login'>Login</NavLink>
-        <NavLink to='/signup'>Sign Up</NavLink>
-      </nav>
-      <main>
-        <Route path="/jokes" component={JokesList} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-      </main>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <nav>
+          <NavLink to='/jokes'>Jokes</NavLink>
+          <NavLink to='/login'>Login</NavLink>
+          <NavLink to='/signup'>Sign Up</NavLink>
+          <button onClick={this.logout}>Log Out</button>
+        </nav>
+        <main>
+          <Route path="/jokes" component={JokesList} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </main>
+      </div>
+    );
+  }
+
+  logout = () => {
+    localStorage.removeItem('jwt');
+
+    this.props.history.push('/login');
+  }
 }
 
 export default withRouter(App);
