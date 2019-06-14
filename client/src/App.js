619e22610ab09abce506ee2f073a0df0d87e91ed
coriagogo <pkcparis@gmail.com>
2019-06-14 12:15:24 -0400

    signup page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { NavLink, Route ,withRouter } from 'react-router-dom';
+
+import SignUp from './components/SignUp';
+
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <NavLink to='/jokes'>Jokes</NavLink>
+        <NavLink to='/login'>Login</NavLink>
+        <NavLink to='/signup'>Sign Up</NavLink>
+      </nav>
+      <main>
+        <Route path="/signup" component={SignUp} />
+      </main>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
