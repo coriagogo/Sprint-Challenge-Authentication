474e8136749d3d61c7a0a2f3f7e9edd253ae19c5
coriagogo <pkcparis@gmail.com>
2019-06-14 11:14:49 -0400

    register user added

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/user-model.js');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -10,7 +14,18 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
-}
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+};
 
 function login(req, res) {
   // implement user login
