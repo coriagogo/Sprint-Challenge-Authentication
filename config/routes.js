c79aedd8bdd423eec879e8675811728f501f7300
coriagogo <pkcparis@gmail.com>
2019-06-14 11:43:26 -0400

    login added and tested

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const Users = require('../users/user-model.js');
+const secrets = require('./secrets.js');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -29,6 +30,38 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    // .where({ username: req.body.username })
+    .then( user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ 
+          message: `Welcome ${user.username}!`,
+          token: `${token}` });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials.' });
+      }
+    })   
+    .catch(error => {
+      res.status(500).json(error);
+    }) 
+};
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  }
+
+  const options = {
+    expiresIn: '1d',
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
 }
 
 function getJokes(req, res) {
